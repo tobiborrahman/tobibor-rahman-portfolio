1f15aa243aa31fbeca6249b09f498b407ee2cb7b
Tobibor Rahman <r.tobibor16@gmail.com>
2023-12-23 21:22:43 +0600

    scroll to top added

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -8,6 +8,7 @@ import Contact from './Contact';
 import { Helmet } from 'react-helmet';
 import Freelancer from './Freelancer';
 import Review from './Review';
+import ScrollTOTop from '../../../shared/Scroll/ScrollTOTop';
 
 const Home = () => {
 	return (
@@ -22,6 +23,7 @@ const Home = () => {
 			<Resume></Resume>
 			<Portfolio></Portfolio>
 			<Review />
+			<ScrollTOTop />
 			<Contact></Contact>
 		</div>
 	);
--- src/shared/Scroll/ScrollTOTop.jsx
+++ src/shared/Scroll/ScrollTOTop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 const ScrollTOTop = () => {
